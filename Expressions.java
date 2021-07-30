555f67cc631b8443a3bca3ad1ef9950452573417
Navian <primeguyy@gmail.com>
2021-07-30 17:28:11 +0000

    expressions

---
+++ Expressions.java
@@ -0,0 +1,32 @@
+
+public class Expressions {
+	
+	public static void main (String[] args) {
+		
+		// expressions are a combination of operands and operators
+		// operands include values, numbers, quantity
+		// operators are + , - , * , ⁄ ,  %
+		
+//		int friends = 10;
+		double friends = 10;
+		
+//		friends = friends - 1;
+//		friends++;
+//		friends--;
+//		System.out.println(friends);
+//		
+//		friends = friends * 3;
+//		System.out.println(friends);
+		
+//		friends = friends / 3; // returns 3 instead of 3.3333 ...
+		
+		// to be able to obtain the decimals in the result,
+		friends = friends / 3;
+		
+		System.out.println(friends);
+		
+		System.out.println(friends);
+		
+	}
+	
+}
