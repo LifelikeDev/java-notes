acfd5cdf5ededefc9d86d56b9a69309cf04d3513
Navian <primeguyy@gmail.com>
2021-09-19 22:52:17 +0000

    reduce but grow

---
+++ ReduceButGrow.java
@@ -0,0 +1,21 @@
+package com.lifelike.dev.codewars_exercises;
+
+public class ReduceButGrow {
+
+//            Given a non-empty array of integers, return the result of multiplying the
+//            values together in order. Example:
+//            [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+    public static int grow(int[] arr){
+        int result = 1;
+        for (int a : arr) {
+            result *= a;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] someArr = {2, 4, 6, 8, 9, 12, 15};
+        System.out.println(grow(someArr));
+    }
+}
