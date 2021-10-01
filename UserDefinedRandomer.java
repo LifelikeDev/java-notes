653612d2aadc967a90809a58938b75faea6a627b
Navian <primeguyy@gmail.com>
2021-10-01 22:42:26 +0000

    generating a random number based off of user input

---
+++ UserDefinedRandomer.java
@@ -0,0 +1,28 @@
+package com.lifelike.dev;
+
+import java.util.Scanner;
+
+public class UserDefinedRandomer {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a lower boundary: ");
+        int lower = scanner.nextInt();
+
+        System.out.println("Enter a upper boundary: ");
+        int upper = scanner.nextInt();
+
+        System.out.println(randomer(lower, upper));
+    }
+
+    private static int randomer(int lower, int upper) {
+
+        int randomNumber = (int) ((Math.random() * upper) + lower);
+
+        System.out.println("Generating a random number between " + lower + " and " + upper + "...");
+        return randomNumber;
+    }
+
+
+}
